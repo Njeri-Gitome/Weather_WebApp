dcba02b30e33864f85c28ac41d890ec85a78f932
Grace Gitome <gitomegracie@gmail.com>
2024-07-04 09:25:46 +0300

    rendering weather conditions

--- server.py
+++ server.py
@@ -10,20 +10,32 @@ app = Flask(__name__)  # define the flask app
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
-# Route to get weather information
+# Get weather data route
 @app.route('/weather')
 def get_weather():
-    city = request.args.get('city')  # retreive city value from the form
+    city = request.args.get('city')
+    
+    #Check for empty string or string with only spaces
+    if not bool(city.strip()): 
+        city = "Nairobi"    # deafault city name if no city is entered
+        
     weather_data = get_current_weather(city)
+    
+    # If city is not found by weather API
+    if not weather_data['cod'] == 200:
+        return render_template ('city-not-found.html')
+    
     return render_template(
-        "weather.html",
+        'weather.html',
         title = weather_data["name"],
         status = weather_data["weather"][0]["description"].capitalize(),
-        temp = f"{weather_data['main']['temp']:.1f}",  # temperature to the nearest 1 decimal place
+        temp = f"{weather_data['main']['temp']:.1f}", # round to 1 decimal place
         feels_like = f"{weather_data['main']['feels_like']:.1f}"
-    )  
+        
+        
+    )
     
 if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=8000)
+    serve(app,host="0.0.0.0", port=8000)
--- weather.py
+++ weather.py
@@ -1,33 +1,30 @@
-import os
-from pprint import pprint
-
-import requests
 from dotenv import load_dotenv
+from pprint import pprint
+import requests
+import os
 
-# Load api key value from the .env file
+load_dotenv() # load api key from .env
 
-load_dotenv()
-
-# get weather function
-
-
-def get_current_weather(city="Nairobi"):
+# Function to get weather data
+def get_current_weather(city = "Nairobi"):
+    
     request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
-   
-    # Get the weather data
+    
     weather_data = requests.get(request_url).json()
+    
     return weather_data
 
+# Main Module
+if __name__ == '__main__':
+    print('\n *** Get Weather Conditions *** \n')
+    
+    city = input('Please enter a City name: ')
+    
+    #Check for empty string or string with only spaces
+    if not bool(city.strip()): 
+        city = "Nairobi"    # deafault city name if no city is entered
 
-# Main module
-if __name__ == "__main__":
-       
-    print("\n *** Get Current Weather Conditions *** \n")
-
-    city = input("\n Please Enter a City name: ")
-    try:
-        weather_data = get_current_weather(city)
-        print("\n")
-        pprint(weather_data)
-    except Exception as e:
-        print(f"Error: {e}")
+    weather_data = get_current_weather(city)
+    
+    print('\n')    
+    pprint(weather_data)
